ae4f40a97ae52bcfae3d302355e5f41c1871bee8
Dinh Hoang <dinhminhhoang235@gmail.com>
2026-02-02 00:02:11 +0700

    fix nativeWind

--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
+  content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {},
